cc9a1c9afd4aca7fd33850422c3cb0353c8a02fa
0xABD01 <Abderrazzak.esl@gmail.com>
2025-10-05 02:15:50 +0100

    v2

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,15 @@ export default defineConfig({
     port: 3000,
     open: true
   },
-  base: '/',
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     rollupOptions: {
       output: {
-        manualChunks: undefined
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
       }
     }
   }
