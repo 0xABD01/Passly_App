df2dcffbbff570cfc5b06508f3537edc7bed5e61
0xABD01 <Abderrazzak.esl@gmail.com>
2025-10-05 02:11:13 +0100

    v2

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Add error boundary for debugging
+console.log('Passly app starting...')
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
